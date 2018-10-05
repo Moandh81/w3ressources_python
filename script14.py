46c28525b56a2c184f1408b497a486379c9d9785
mohand81 <anis.dhouieb@gmail.com>
2018-10-05 11:02:45 +0200

    script  14

---
+++ script14.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+
+#Python Data Type: String - Exercises
+
+# Write a Python script that takes input from the user and displays that input back in upper and lower case
+
+chaine = "Ceci est une chaine de caracteres"
+
+print(lower.chaine)
+print(upper.chaine)
